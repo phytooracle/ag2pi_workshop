787b2ef9f8e6d73c83a422b9b848113017aec8c4
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-07-21 23:04:18 -0700

    v1

---
+++ download_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+wget https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/workshops/ag2pi/flir/2021-07-28_data.tar.gz
+wget https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/workshops/ag2pi/flir/model_weights_sorghum_flir.pth
+wget https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/workshops/ag2pi/hsi/model_weights_panel_2021-06-18_transform.pth
+wget https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/workshops/ag2pi/hsi/sample.h5
+
